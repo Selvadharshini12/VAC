a2967004d93b7a4aa91c00ce5a535dd346d9ea53
Nini <selvadharshiniselvadharshioni9@gmail.com>
2025-06-06 15:17:32 +0530

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+    function greet(name,age){
+        return `Hello ${name} is ${age} old`
+    }
+    console.log(greet ('Ronaldo',45));
+    function add (a,b){
+        return `sum is ${a+b}`
+    }
+    console.log(add(4,5));
+    
+    
+
